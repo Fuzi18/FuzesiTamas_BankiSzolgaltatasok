1c9d6bbbaaba15fb278ad42016903f1a0b57726e
Fuzi18 <fuzesi.tamas@petrik.hu>
2022-10-13 10:24:01 +0200

    6.feladat

--- src/hu/petrik/bankiszolgaltatas/MegtakaritasiSzamla.java
+++ src/hu/petrik/bankiszolgaltatas/MegtakaritasiSzamla.java
@@ -1,4 +1,32 @@
 package hu.petrik.bankiszolgaltatas;
 
-public class MegtakaritasiSzamla {
+public class MegtakaritasiSzamla extends Szamla {
+    private double kamat;
+    public static final double alapKamat = 1.1;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        kamat = alapKamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    public boolean kivesz(int osszeg) {
+        boolean sikeresKivetel = false;
+        if (aktualisEgyenleg - osszeg >= 0) {
+            aktualisEgyenleg -= osszeg;
+            sikeresKivetel = true;
+        }
+        return sikeresKivetel;
+    }
+
+    public void kamatJovairas() {
+        aktualisEgyenleg = (int) (kamat * aktualisEgyenleg);
+    }
 }
